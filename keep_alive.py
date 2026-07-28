fa783b083279fafbdfb26990c43d91f989dc14c2
nessy <92063760+celedriel@users.noreply.github.com>
2026-07-28 20:23:22 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,12 @@
 import tornado.web
-
 import logging
+
 logging.getLogger('tornado.access').disabled = True
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(""parabéns amigo, você é um amigo!")
+        self.write("parabéns amigo, você é um amigo!")
 
 def keep_alive():
-  app = tornado.web.Application([(r"/", MainHandler)])
-  app.listen(8080)
+    app = tornado.web.Application([(r"/", MainHandler)])
+    app.listen(8080)
